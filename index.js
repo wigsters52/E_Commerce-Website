daa25d014243486ea060d878132a704bdd708786
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-03-14 17:55:20 -0400

    added middleware

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true }))
 app.get('/', (req, res) => {
-  res.send('hi there')
+  res.send(`
+  <div>
+    <form method="POST">
+        <input name="email" placeholder="email"/>
+        <input name="password" placeholder="password"/>
+        <input name="passwordConfirmation" placeholder="password confirmation"/>
+        <button>Sign Up!</button>
+    </form>
+  </div>
+  `)
 })
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('Account Created!!!!')
+})
 app.listen(3000, () => {
   console.log('Listening')
 })
