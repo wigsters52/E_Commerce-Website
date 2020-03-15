f512ed3a4ce1fa0875b4e0b4f8084db6d1e6c673
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-03-15 14:58:25 -0400

    create user function added

--- repositories/users.js
+++ repositories/users.js
@@ -13,7 +13,29 @@ class UsersRepository {
     }
   }
 
-  async checkForFile () {}
+  async getAll () {
+    // open the file called this.filename
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, {
+        encoding: 'utf8'
+      })
+    )
+  }
+
+  async create (attrs) {
+    // object that has an email and password
+    const records = await this.getAll()
+    records.push(attrs)
+    // write the updated 'records array back to this.filename
+    await fs.promises.writeFile(this.filename, JSON.stringify(records))
+  }
 }
 
-const repo = new UsersRepository('users.json')
+const test = async () => {
+  const repo = new UsersRepository('users.json')
+  await repo.create({ email: 'test@test.com', password: 'password' })
+  const users = await repo.getAll()
+  console.log(users)
+}
+
+test()
