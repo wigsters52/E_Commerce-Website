95ebbfe4dbdce2d73a35bddadea993f33a0178ab
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-03-15 16:12:16 -0400

    basic form validation added

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const usersRepo = require('./repositories/users')
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.get('/', (req, res) => {
   res.send(`
   <div>
@@ -17,8 +19,15 @@ app.get('/', (req, res) => {
   `)
 })
 
-app.post('/', (req, res) => {
-  console.log(req.body)
+app.post('/', async (req, res) => {
+  const { email, password, passwordConfirmation } = req.body
+  const existingUser = await usersRepo.getOneBy({ email })
+  if (existingUser) {
+    return res.send('Email in use')
+  }
+  if (password !== passwordConfirmation) {
+    return res.send('Passwords must match!')
+  }
   res.send('Account Created!!!!')
 })
 app.listen(3000, () => {
