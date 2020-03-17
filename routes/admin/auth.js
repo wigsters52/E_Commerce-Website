ae11b4a35cb46d1599abd2c2a352103f30b6ccbb
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-03-17 16:01:04 -0400

    added error messages to html page

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const { check, validationResult } = require('express-validator')
+
 const usersRepo = require('../../repositories/users')
 const signupTemplate = require('../../views/admin/auth/signup')
 const signinTemplate = require('../../views/admin/auth/signin')
+const {
+  requireEmail,
+  requirePassword,
+  requirePasswordConfirmation
+} = require('./validators')
+
 const router = express.Router()
 
 router.get('/signup', (req, res) => {
@@ -11,42 +18,26 @@ router.get('/signup', (req, res) => {
 
 router.post(
   '/signup',
-  [
-    check('email')
-      .trim()
-      .normalizeEmail()
-      .isEmail(),
-    check('password')
-      .trim()
-      .isLength({ min: 4, max: 20 }),
-    check('passwordConfirmation')
-      .trim()
-      .isLength({ min: 4, max: 20 })
-  ],
+  [requireEmail, requirePassword, requirePasswordConfirmation],
   async (req, res) => {
     const errors = validationResult(req)
-    console.log(errors)
+
+    if (!errors.isEmpty()) {
+      return res.send(signupTemplate({ req, errors }))
+    }
+
     const { email, password, passwordConfirmation } = req.body
-    const existingUser = await usersRepo.getOneBy({ email })
-    if (existingUser) {
-      return res.send('Email in use')
-    }
-    if (password !== passwordConfirmation) {
-      return res.send('Passwords must match!')
-    }
-    // create a user in our user repo to represent this person
     const user = await usersRepo.create({ email, password })
 
-    // Store the id of that user inside the users cookie
     req.session.userId = user.id
 
-    res.send('Account Created!!!!')
+    res.send('Account created!!!')
   }
 )
 
 router.get('/signout', (req, res) => {
   req.session = null
-  res.send('You are logged out!')
+  res.send('You are logged out')
 })
 
 router.get('/signin', (req, res) => {
@@ -59,8 +50,9 @@ router.post('/signin', async (req, res) => {
   const user = await usersRepo.getOneBy({ email })
 
   if (!user) {
-    return res.send('Email not found!')
+    return res.send('Email not found')
   }
+
   const validPassword = await usersRepo.comparePasswords(
     user.password,
     password
@@ -71,7 +63,7 @@ router.post('/signin', async (req, res) => {
 
   req.session.userId = user.id
 
-  res.send('You are signed in!')
+  res.send('You are signed in!!!')
 })
 
 module.exports = router
--- views/admin/auth/signup.js
+++ views/admin/auth/signup.js
@@ -1,16 +1,28 @@
 const layout = require('../layout')
-module.exports = ({ req }) => {
+
+const getError = (errors, prop) => {
+  try {
+    return errors.mapped()[prop].msg
+  } catch (err) {
+    return ''
+  }
+}
+
+module.exports = ({ req, errors }) => {
   return layout({
     content: `
     <div>
-      Your id is ${req.session.userId}
+      Your id is: ${req.session.userId}
       <form method="POST">
-          <input name="email" placeholder="email"/>
-          <input name="password" placeholder="password"/>
-          <input name="passwordConfirmation" placeholder="password confirmation"/>
-          <button>Sign Up!</button>
+        <input name="email" placeholder="email" />
+        ${getError(errors, 'email')}
+        <input name="password" placeholder="password" />
+        ${getError(errors, 'password')}
+        <input name="passwordConfirmation" placeholder="password confirmation" />
+        ${getError(errors, 'passwordConfirmation')}
+        <button>Sign Up</button>
       </form>
     </div>
-    `
+  `
   })
 }
